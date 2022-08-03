008022b6d96593041074064a26adeaf84f1a8e65
Guilherme Medeiros <guilherme.medeiros@platformbuilders.io>
2022-08-03 15:24:53 +0100

    Criar get users

--- src/main/java/me/guilherme/quarkussocial/rest/UserResource.java
+++ src/main/java/me/guilherme/quarkussocial/rest/UserResource.java
@@ -2,18 +2,25 @@ package me.guilherme.quarkussocial.rest;
 
 import me.guilherme.quarkussocial.rest.dto.CreateUserRequest;
 
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
 
     @POST
     public Response createUser(CreateUserRequest userRequest) {
         return Response
-                .ok()
+                .ok(userRequest)
                 .build();
 
+    }
+
+    @GET
+    public Response listAllUsers(){
+        return Response.ok().build();
     }
 }
