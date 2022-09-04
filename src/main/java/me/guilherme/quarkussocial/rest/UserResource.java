8756bf71e17a0891c8a943719378712f780568b7
Guilherme Medeiros <guilherme.medeiros@platformbuilders.io>
2022-09-04 15:08:09 +0100

    Criar update de usuario

--- src/main/java/me/guilherme/quarkussocial/rest/UserResource.java
+++ src/main/java/me/guilherme/quarkussocial/rest/UserResource.java
@@ -33,4 +33,35 @@ public class UserResource {
         PanacheQuery<User> query = User.findAll();
         return Response.ok(query.list()).build();
     }
+
+
+    @Transactional
+    @DELETE
+    @Path("/{id}")
+    public Response deleteUser(@PathParam("id") Long id) {
+        User user = User.findById(id);
+
+        if (user != null) {
+            user.delete();
+            return Response.ok()
+                    .build();
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
+
+    }
+
+
+    @Path("{id}")
+    @PUT
+    @Transactional
+    public Response updateUser(@PathParam("id") Long id, CreateUserRequest userData) {
+        User user = User.findById(id);
+
+        if (user != null) {
+            user.setName(userData.getName());
+            user.setAge(userData.getAge());
+            return Response.ok().build();
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
 }
